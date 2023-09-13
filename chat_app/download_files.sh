7ca06c15125ad5175c435dd19cb98a4f5ae9259f
Caio Benatti Moretti <caiodba@gmail.com>
2023-09-13 14:54:13 +0200

    updating script to download files for participants

--- chat_app/download_files.sh
+++ chat_app/download_files.sh
@@ -1,1 +1,4 @@
+wget -cO - https://raw.githubusercontent.com/binxio/workshop-bindinc-api/participant/chat_app/notebooks/ingestion.ipynb?token=GHSAT0AAAAAACEUQAP2VY3OKBC5CEJOATKYZIBV63Q > ingestion.ipynb
+wget -cO - https://raw.githubusercontent.com/binxio/workshop-bindinc-api/participant/chat_app/pyproject.toml?token=GHSAT0AAAAAACEUQAP26URGHGJZHPGCQV7EZIBWAJQ > pyproject.toml
+wget -cO - https://raw.githubusercontent.com/binxio/workshop-bindinc-api/participant/data/raw/binge_apporved_animation.json?token=GHSAT0AAAAAACEUQAP3FNKPJIFV2YOEINWKZIBV4QQ > binge_apporved_animation.json
 
