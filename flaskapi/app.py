b0412b05b2ef2affdeef0c8259fbacf2c17d500c
Dennis Vink <dennisvink@binx.io>
2023-09-13 17:12:45 +0200

    Read POST data and return information from the vector database

--- flaskapi/app.py
+++ flaskapi/app.py
@@ -1,11 +1,11 @@
 import json
+#import vertexai # uncomment when you want to extend functionality and leverage GCP vertexai services
 from langchain.vectorstores.faiss import FAISS
 from langchain.embeddings.vertexai import VertexAIEmbeddings
 from flask import Flask, render_template, send_from_directory, request, jsonify
 
 app = Flask(__name__)
 
-# Load the vector database
 embeddings = VertexAIEmbeddings(
     model_name="textembedding-gecko-multilingual@latest",
     chunk_size=1
@@ -17,10 +17,19 @@ store = FAISS.load_local("data/index", embeddings, "index")
 def home():
     return jsonify({'OK': '200 OK'}), 200
 
-# Still serve mock response
 @app.route('/similarity', methods=['POST'])
 def get_similarity():
-    output = {"status": "Pretending to have processed your data and returning this result."}
+    # Get the JSON data from POST request
+    data = request.get_json()
+
+    # Ensure the data contains a 'query' key
+    if not data or 'query' not in data:
+        return jsonify({'error': 'Missing query parameter'}), 400
+
+    query = data['query']
+    result = store.similarity_search(query)
+
+    output = [{"chunk": r.page_content, "metadata": r.metadata} for r in result]
     return jsonify(output), 200
 
 if __name__ == "__main__":
