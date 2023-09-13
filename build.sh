e351f79f66746053c3992a08381fba62711e00c5
Caio Benatti Moretti <caiodba@gmail.com>
2023-09-13 14:32:14 +0200

    adding participant material

--- build.sh
+++
@@ -1,4 +0,0 @@
-docker buildx build . --platform linux/amd64 -t gcp-bindincapi
-docker tag gcp-bindincapi gcr.io/playground-dennisvink/gcp-bindincapi
-docker push gcr.io/playground-dennisvink/gcp-bindincapi
-gcloud run deploy gcp-bindincapi --image gcr.io/playground-dennisvink/gcp-bindincapi --platform managed --region us-east1 --allow-unauthenticated --timeout=900
