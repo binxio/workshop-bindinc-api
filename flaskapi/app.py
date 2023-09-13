59e6a6d14cf645a99c263a7e0a8dd86d547f6593
Dennis Vink <dennisvink@binx.io>
2023-09-13 16:56:53 +0200

    Removed unnecessary imports

--- flaskapi/app.py
+++ flaskapi/app.py
@@ -1,12 +1,8 @@
-import base64
 import json
-import os
-import uuid
-import subprocess
+from flask import Flask, render_template, send_from_directory, request, jsonify
 import vertexai
 from langchain.vectorstores.faiss import FAISS
 from langchain.embeddings.vertexai import VertexAIEmbeddings
-from flask import Flask, render_template, send_from_directory, request, jsonify
 
 app = Flask(__name__)
 
