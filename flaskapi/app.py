d4e7cb20dbd1f515dfa2eebe746e76cf6f1c5994
Dennis Vink <dennisvink@binx.io>
2023-09-13 17:06:33 +0200

    Adding POST route

--- flaskapi/app.py
+++ flaskapi/app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def home():
     return jsonify({'OK': '200 OK'}), 200
 
+@app.route('/similarity', methods=['POST'])
+def get_similarity():
+    output = {"status": "Pretending to have processed your data and returning this result."}
+    return jsonify(output), 200
+
 if __name__ == "__main__":
     app.run()
