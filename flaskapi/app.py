bfc9f6d3e5722cce439e8120132bee26cf3248c5
Dennis Vink <dennisvink@binx.io>
2023-09-13 17:10:04 +0200

    Load vector database

--- flaskapi/app.py
+++ flaskapi/app.py
@@ -1,12 +1,23 @@
 import json
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings.vertexai import VertexAIEmbeddings
 from flask import Flask, render_template, send_from_directory, request, jsonify
 
 app = Flask(__name__)
+
+# Load the vector database
+embeddings = VertexAIEmbeddings(
+    model_name="textembedding-gecko-multilingual@latest",
+    chunk_size=1
+)
+
+store = FAISS.load_local("data/index", embeddings, "index")
 
 @app.route('/')
 def home():
     return jsonify({'OK': '200 OK'}), 200
 
+# Still serve mock response
 @app.route('/similarity', methods=['POST'])
 def get_similarity():
     output = {"status": "Pretending to have processed your data and returning this result."}
